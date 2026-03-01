e192a01879b7c0c9e3bbdaf63e1a7bccad52dcad
douyo01-byte <douyo01@gmail.com>
2026-03-01 22:08:41 +0900

    auto: auto-20260301220839

--- autogen.py
+++ autogen.py
@@ -1,13 +1,4 @@
 ```python
-class NewAuto:
-    def __init__(self, make: str, model: str, year: int):
-        self.make = make
-        self.model = model
-        self.year = year
-
-    def __repr__(self):
-        return f"NewAuto(make={self.make!r}, model={self.model!r}, year={self.year!r})"
-
-    def age(self, current_year: int) -> int:
-        return current_year - self.year
+class NewAuto2:
+    pass
 ```
