79d9584dc7cb74d37927574c24a2841e648a20af
douyo01-byte <douyo01@gmail.com>
2026-03-01 15:22:39 +0900

    dev_proposal_generator: fix branch concat + datetime

--- bots/dev_proposal_generator_v1.py
+++ bots/dev_proposal_generator_v1.py
@@ -7,16 +7,16 @@ IDEAS=[
 ("Optimize Costs","Add cost estimator","dev/idea-b"),
 ("Speed Pipeline","Parallelize jobs","dev/idea-c"),]
 def main():
-    pid=int(datetime.datetime.utcnow().timestamp())
+    pid=int(datetime.datetime.now(datetime.UTC).timestamp())
 
     pid=None
 
     conn=sqlite3.connect(DB)
     cur=conn.cursor()
-    pid=int(datetime.datetime.utcnow().timestamp())
+    pid=int(datetime.datetime.now(datetime.UTC).timestamp())
     t=random.choice(IDEAS)
     cur.execute("""insert into dev_proposals(title,description,branch_name,status)
-                   values(?,?,?,?)""",(t[0],t[1],(t[2]%pid),"approved"))
+                   values(?,?,?,?)""",(t[0],t[1],(t[2]+"-"+str(pid)),"approved"))
     conn.commit()
     print("proposal created")
 
